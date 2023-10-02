223c8d52b721e5f3ecefefa9718c2b7718d266e9
_x128 <143216072+XanderXander123@users.noreply.github.com>
2023-10-02 04:26:41 -0700

    Add files via upload

---
+++ cmd.exe.c
@@ -0,0 +1,500 @@
+typedef unsigned char   undefined;
+
+typedef unsigned long long    GUID;
+typedef pointer32 ImageBaseOffset32;
+
+typedef pointer64 ImageBaseOffset64;
+
+typedef unsigned char    byte;
+typedef unsigned int    dword;
+typedef unsigned long long    qword;
+typedef unsigned short    word;
+typedef unsigned short    wchar16;
+typedef union IMAGE_RESOURCE_DIRECTORY_ENTRY_DirectoryUnion IMAGE_RESOURCE_DIRECTORY_ENTRY_DirectoryUnion, *PIMAGE_RESOURCE_DIRECTORY_ENTRY_DirectoryUnion;
+
+typedef struct IMAGE_RESOURCE_DIRECTORY_ENTRY_DirectoryStruct IMAGE_RESOURCE_DIRECTORY_ENTRY_DirectoryStruct, *PIMAGE_RESOURCE_DIRECTORY_ENTRY_DirectoryStruct;
+
+struct IMAGE_RESOURCE_DIRECTORY_ENTRY_DirectoryStruct {
+    dword OffsetToDirectory;
+    dword DataIsDirectory;
+};
+
+union IMAGE_RESOURCE_DIRECTORY_ENTRY_DirectoryUnion {
+    dword OffsetToData;
+    struct IMAGE_RESOURCE_DIRECTORY_ENTRY_DirectoryStruct IMAGE_RESOURCE_DIRECTORY_ENTRY_DirectoryStruct;
+};
+
+typedef struct _IMAGE_RUNTIME_FUNCTION_ENTRY _IMAGE_RUNTIME_FUNCTION_ENTRY, *P_IMAGE_RUNTIME_FUNCTION_ENTRY;
+
+struct _IMAGE_RUNTIME_FUNCTION_ENTRY {
+    ImageBaseOffset32 BeginAddress;
+    ImageBaseOffset32 EndAddress;
+    ImageBaseOffset32 UnwindInfoAddressOrData;
+};
+
+typedef struct Var Var, *PVar;
+
+struct Var {
+    word wLength;
+    word wValueLength;
+    word wType;
+};
+
+typedef struct IMAGE_RESOURCE_DIRECTORY_ENTRY_NameStruct IMAGE_RESOURCE_DIRECTORY_ENTRY_NameStruct, *PIMAGE_RESOURCE_DIRECTORY_ENTRY_NameStruct;
+
+struct IMAGE_RESOURCE_DIRECTORY_ENTRY_NameStruct {
+    dword NameOffset;
+    dword NameIsString;
+};
+
+typedef struct IMAGE_LOAD_CONFIG_CODE_INTEGRITY IMAGE_LOAD_CONFIG_CODE_INTEGRITY, *PIMAGE_LOAD_CONFIG_CODE_INTEGRITY;
+
+struct IMAGE_LOAD_CONFIG_CODE_INTEGRITY {
+    word Flags;
+    word Catalog;
+    dword CatalogOffset;
+    dword Reserved;
+};
+
+typedef struct IMAGE_IMPORT_BY_NAME_12 IMAGE_IMPORT_BY_NAME_12, *PIMAGE_IMPORT_BY_NAME_12;
+
+struct IMAGE_IMPORT_BY_NAME_12 {
+    word Hint;
+    char Name[12];
+};
+
+typedef struct IMAGE_DEBUG_DIRECTORY IMAGE_DEBUG_DIRECTORY, *PIMAGE_DEBUG_DIRECTORY;
+
+struct IMAGE_DEBUG_DIRECTORY {
+    dword Characteristics;
+    dword TimeDateStamp;
+    word MajorVersion;
+    word MinorVersion;
+    dword Type;
+    dword SizeOfData;
+    dword AddressOfRawData;
+    dword PointerToRawData;
+};
+
+typedef struct IMAGE_IMPORT_BY_NAME_16 IMAGE_IMPORT_BY_NAME_16, *PIMAGE_IMPORT_BY_NAME_16;
+
+struct IMAGE_IMPORT_BY_NAME_16 {
+    word Hint;
+    char Name[16];
+};
+
+typedef struct IMAGE_IMPORT_BY_NAME_17 IMAGE_IMPORT_BY_NAME_17, *PIMAGE_IMPORT_BY_NAME_17;
+
+struct IMAGE_IMPORT_BY_NAME_17 {
+    word Hint;
+    char Name[17];
+};
+
+typedef struct IMAGE_IMPORT_BY_NAME_19 IMAGE_IMPORT_BY_NAME_19, *PIMAGE_IMPORT_BY_NAME_19;
+
+struct IMAGE_IMPORT_BY_NAME_19 {
+    word Hint;
+    char Name[19];
+};
+
+typedef struct StringTable StringTable, *PStringTable;
+
+struct StringTable {
+    word wLength;
+    word wValueLength;
+    word wType;
+};
+
+typedef struct IMAGE_SECTION_HEADER IMAGE_SECTION_HEADER, *PIMAGE_SECTION_HEADER;
+
+typedef union Misc Misc, *PMisc;
+
+typedef enum SectionFlags {
+    IMAGE_SCN_TYPE_NO_PAD=8,
+    IMAGE_SCN_RESERVED_0001=16,
+    IMAGE_SCN_CNT_CODE=32,
+    IMAGE_SCN_CNT_INITIALIZED_DATA=64,
+    IMAGE_SCN_CNT_UNINITIALIZED_DATA=128,
+    IMAGE_SCN_LNK_OTHER=256,
+    IMAGE_SCN_LNK_INFO=512,
+    IMAGE_SCN_RESERVED_0040=1024,
+    IMAGE_SCN_LNK_REMOVE=2048,
+    IMAGE_SCN_LNK_COMDAT=4096,
+    IMAGE_SCN_GPREL=32768,
+    IMAGE_SCN_MEM_16BIT=131072,
+    IMAGE_SCN_MEM_PURGEABLE=131072,
+    IMAGE_SCN_MEM_LOCKED=262144,
+    IMAGE_SCN_MEM_PRELOAD=524288,
+    IMAGE_SCN_ALIGN_1BYTES=1048576,
+    IMAGE_SCN_ALIGN_2BYTES=2097152,
+    IMAGE_SCN_ALIGN_4BYTES=3145728,
+    IMAGE_SCN_ALIGN_8BYTES=4194304,
+    IMAGE_SCN_ALIGN_16BYTES=5242880,
+    IMAGE_SCN_ALIGN_32BYTES=6291456,
+    IMAGE_SCN_ALIGN_64BYTES=7340032,
+    IMAGE_SCN_ALIGN_128BYTES=8388608,
+    IMAGE_SCN_ALIGN_256BYTES=9437184,
+    IMAGE_SCN_ALIGN_512BYTES=10485760,
+    IMAGE_SCN_ALIGN_1024BYTES=11534336,
+    IMAGE_SCN_ALIGN_2048BYTES=12582912,
+    IMAGE_SCN_ALIGN_4096BYTES=13631488,
+    IMAGE_SCN_ALIGN_8192BYTES=14680064,
+    IMAGE_SCN_LNK_NRELOC_OVFL=16777216,
+    IMAGE_SCN_MEM_DISCARDABLE=33554432,
+    IMAGE_SCN_MEM_NOT_CACHED=67108864,
+    IMAGE_SCN_MEM_NOT_PAGED=134217728,
+    IMAGE_SCN_MEM_SHARED=268435456,
+    IMAGE_SCN_MEM_EXECUTE=536870912,
+    IMAGE_SCN_MEM_READ=1073741824,
+    IMAGE_SCN_MEM_WRITE=2147483648
+} SectionFlags;
+
+union Misc {
+    dword PhysicalAddress;
+    dword VirtualSize;
+};
+
+struct IMAGE_SECTION_HEADER {
+    char Name[8];
+    union Misc Misc;
+    ImageBaseOffset32 VirtualAddress;
+    dword SizeOfRawData;
+    dword PointerToRawData;
+    dword PointerToRelocations;
+    dword PointerToLinenumbers;
+    word NumberOfRelocations;
+    word NumberOfLinenumbers;
+    enum SectionFlags Characteristics;
+};
+
+typedef struct IMAGE_NT_HEADERS64 IMAGE_NT_HEADERS64, *PIMAGE_NT_HEADERS64;
+
+typedef struct IMAGE_FILE_HEADER IMAGE_FILE_HEADER, *PIMAGE_FILE_HEADER;
+
+typedef struct IMAGE_OPTIONAL_HEADER64 IMAGE_OPTIONAL_HEADER64, *PIMAGE_OPTIONAL_HEADER64;
+
+typedef struct IMAGE_DATA_DIRECTORY IMAGE_DATA_DIRECTORY, *PIMAGE_DATA_DIRECTORY;
+
+struct IMAGE_DATA_DIRECTORY {
+    ImageBaseOffset32 VirtualAddress;
+    dword Size;
+};
+
+struct IMAGE_OPTIONAL_HEADER64 {
+    word Magic;
+    byte MajorLinkerVersion;
+    byte MinorLinkerVersion;
+    dword SizeOfCode;
+    dword SizeOfInitializedData;
+    dword SizeOfUninitializedData;
+    ImageBaseOffset32 AddressOfEntryPoint;
+    ImageBaseOffset32 BaseOfCode;
+    pointer64 ImageBase;
+    dword SectionAlignment;
+    dword FileAlignment;
+    word MajorOperatingSystemVersion;
+    word MinorOperatingSystemVersion;
+    word MajorImageVersion;
+    word MinorImageVersion;
+    word MajorSubsystemVersion;
+    word MinorSubsystemVersion;
+    dword Win32VersionValue;
+    dword SizeOfImage;
+    dword SizeOfHeaders;
+    dword CheckSum;
+    word Subsystem;
+    word DllCharacteristics;
+    qword SizeOfStackReserve;
+    qword SizeOfStackCommit;
+    qword SizeOfHeapReserve;
+    qword SizeOfHeapCommit;
+    dword LoaderFlags;
+    dword NumberOfRvaAndSizes;
+    struct IMAGE_DATA_DIRECTORY DataDirectory[16];
+};
+
+struct IMAGE_FILE_HEADER {
+    word Machine; // 34404
+    word NumberOfSections;
+    dword TimeDateStamp;
+    dword PointerToSymbolTable;
+    dword NumberOfSymbols;
+    word SizeOfOptionalHeader;
+    word Characteristics;
+};
+
+struct IMAGE_NT_HEADERS64 {
+    char Signature[4];
+    struct IMAGE_FILE_HEADER FileHeader;
+    struct IMAGE_OPTIONAL_HEADER64 OptionalHeader;
+};
+
+typedef struct IMAGE_RESOURCE_DIR_STRING_U_6 IMAGE_RESOURCE_DIR_STRING_U_6, *PIMAGE_RESOURCE_DIR_STRING_U_6;
+
+struct IMAGE_RESOURCE_DIR_STRING_U_6 {
+    word Length;
+    wchar16 NameString[3];
+};
+
+typedef struct IMAGE_RESOURCE_DATA_ENTRY IMAGE_RESOURCE_DATA_ENTRY, *PIMAGE_RESOURCE_DATA_ENTRY;
+
+struct IMAGE_RESOURCE_DATA_ENTRY {
+    dword OffsetToData;
+    dword Size;
+    dword CodePage;
+    dword Reserved;
+};
+
+typedef struct IMAGE_IMPORT_BY_NAME_21 IMAGE_IMPORT_BY_NAME_21, *PIMAGE_IMPORT_BY_NAME_21;
+
+struct IMAGE_IMPORT_BY_NAME_21 {
+    word Hint;
+    char Name[21];
+};
+
+typedef struct IMAGE_IMPORT_BY_NAME_20 IMAGE_IMPORT_BY_NAME_20, *PIMAGE_IMPORT_BY_NAME_20;
+
+struct IMAGE_IMPORT_BY_NAME_20 {
+    word Hint;
+    char Name[20];
+};
+
+typedef struct IMAGE_RESOURCE_DIRECTORY IMAGE_RESOURCE_DIRECTORY, *PIMAGE_RESOURCE_DIRECTORY;
+
+struct IMAGE_RESOURCE_DIRECTORY {
+    dword Characteristics;
+    dword TimeDateStamp;
+    word MajorVersion;
+    word MinorVersion;
+    word NumberOfNamedEntries;
+    word NumberOfIdEntries;
+};
+
+typedef struct IMAGE_IMPORT_BY_NAME_23 IMAGE_IMPORT_BY_NAME_23, *PIMAGE_IMPORT_BY_NAME_23;
+
+struct IMAGE_IMPORT_BY_NAME_23 {
+    word Hint;
+    char Name[23];
+};
+
+typedef struct IMAGE_IMPORT_BY_NAME_22 IMAGE_IMPORT_BY_NAME_22, *PIMAGE_IMPORT_BY_NAME_22;
+
+struct IMAGE_IMPORT_BY_NAME_22 {
+    word Hint;
+    char Name[22];
+};
+
+typedef struct IMAGE_IMPORT_BY_NAME_24 IMAGE_IMPORT_BY_NAME_24, *PIMAGE_IMPORT_BY_NAME_24;
+
+struct IMAGE_IMPORT_BY_NAME_24 {
+    word Hint;
+    char Name[24];
+};
+
+typedef struct IMAGE_IMPORT_BY_NAME_27 IMAGE_IMPORT_BY_NAME_27, *PIMAGE_IMPORT_BY_NAME_27;
+
+struct IMAGE_IMPORT_BY_NAME_27 {
+    word Hint;
+    char Name[27];
+};
+
+typedef struct IMAGE_IMPORT_BY_NAME_29 IMAGE_IMPORT_BY_NAME_29, *PIMAGE_IMPORT_BY_NAME_29;
+
+struct IMAGE_IMPORT_BY_NAME_29 {
+    word Hint;
+    char Name[29];
+};
+
+typedef union IMAGE_RESOURCE_DIRECTORY_ENTRY_NameUnion IMAGE_RESOURCE_DIRECTORY_ENTRY_NameUnion, *PIMAGE_RESOURCE_DIRECTORY_ENTRY_NameUnion;
+
+union IMAGE_RESOURCE_DIRECTORY_ENTRY_NameUnion {
+    struct IMAGE_RESOURCE_DIRECTORY_ENTRY_NameStruct IMAGE_RESOURCE_DIRECTORY_ENTRY_NameStruct;
+    dword Name;
+    word Id;
+};
+
+typedef struct IMAGE_IMPORT_BY_NAME_31 IMAGE_IMPORT_BY_NAME_31, *PIMAGE_IMPORT_BY_NAME_31;
+
+struct IMAGE_IMPORT_BY_NAME_31 {
+    word Hint;
+    char Name[31];
+};
+
+typedef struct IMAGE_LOAD_CONFIG_DIRECTORY64 IMAGE_LOAD_CONFIG_DIRECTORY64, *PIMAGE_LOAD_CONFIG_DIRECTORY64;
+
+typedef enum IMAGE_GUARD_FLAGS {
+    IMAGE_GUARD_CF_INSTRUMENTED=256,
+    IMAGE_GUARD_CFW_INSTRUMENTED=512,
+    IMAGE_GUARD_CF_FUNCTION_TABLE_PRESENT=1024,
+    IMAGE_GUARD_SECURITY_COOKIE_UNUSED=2048,
+    IMAGE_GUARD_PROTECT_DELAYLOAD_IAT=4096,
+    IMAGE_GUARD_DELAYLOAD_IAT_IN_ITS_OWN_SECTION=8192,
+    IMAGE_GUARD_CF_EXPORT_SUPPRESSION_INFO_PRESENT=16384,
+    IMAGE_GUARD_CF_ENABLE_EXPORT_SUPPRESSION=32768,
+    IMAGE_GUARD_CF_LONGJUMP_TABLE_PRESENT=65536,
+    IMAGE_GUARD_RF_INSTRUMENTED=131072,
+    IMAGE_GUARD_RF_ENABLE=262144,
+    IMAGE_GUARD_RF_STRICT=524288,
+    IMAGE_GUARD_CF_FUNCTION_TABLE_SIZE_MASK_1=268435456,
+    IMAGE_GUARD_CF_FUNCTION_TABLE_SIZE_MASK_2=536870912,
+    IMAGE_GUARD_CF_FUNCTION_TABLE_SIZE_MASK_4=1073741824,
+    IMAGE_GUARD_CF_FUNCTION_TABLE_SIZE_MASK_8=2147483648
+} IMAGE_GUARD_FLAGS;
+
+struct IMAGE_LOAD_CONFIG_DIRECTORY64 {
+    dword Size;
+    dword TimeDateStamp;
+    word MajorVersion;
+    word MinorVersion;
+    dword GlobalFlagsClear;
+    dword GlobalFlagsSet;
+    dword CriticalSectionDefaultTimeout;
+    qword DeCommitFreeBlockThreshold;
+    qword DeCommitTotalFreeThreshold;
+    pointer64 LockPrefixTable;
+    qword MaximumAllocationSize;
+    qword VirtualMemoryThreshold;
+    qword ProcessAffinityMask;
+    dword ProcessHeapFlags;
+    word CsdVersion;
+    word DependentLoadFlags;
+    pointer64 EditList;
+    pointer64 SecurityCookie;
+    pointer64 SEHandlerTable;
+    qword SEHandlerCount;
+    pointer64 GuardCFCCheckFunctionPointer;
+    pointer64 GuardCFDispatchFunctionPointer;
+    pointer64 GuardCFFunctionTable;
+    qword GuardCFFunctionCount;
+    enum IMAGE_GUARD_FLAGS GuardFlags;
+    struct IMAGE_LOAD_CONFIG_CODE_INTEGRITY CodeIntegrity;
+    pointer64 GuardAddressTakenIatEntryTable;
+    qword GuardAddressTakenIatEntryCount;
+    pointer64 GuardLongJumpTargetTable;
+    qword GuardLongJumpTargetCount;
+    pointer64 DynamicValueRelocTable;
+    pointer64 CHPEMetadataPointer;
+    pointer64 GuardRFFailureRoutine;
+    pointer64 GuardRFFailureRoutineFunctionPointer;
+    dword DynamicValueRelocTableOffset;
+    word DynamicValueRelocTableSection;
+    word Reserved1;
+    pointer64 GuardRFVerifyStackPointerFunctionPointer;
+    dword HotPatchTableOffset;
+    dword Reserved2;
+    qword Reserved3;
+};
+
+typedef struct StringFileInfo StringFileInfo, *PStringFileInfo;
+
+struct StringFileInfo {
+    word wLength;
+    word wValueLength;
+    word wType;
+};
+
+typedef union IMAGE_RESOURCE_DIRECTORY_ENTRY IMAGE_RESOURCE_DIRECTORY_ENTRY, *PIMAGE_RESOURCE_DIRECTORY_ENTRY;
+
+union IMAGE_RESOURCE_DIRECTORY_ENTRY {
+    union IMAGE_RESOURCE_DIRECTORY_ENTRY_NameUnion NameUnion;
+    union IMAGE_RESOURCE_DIRECTORY_ENTRY_DirectoryUnion DirectoryUnion;
+};
+
+typedef struct ImgDelayDescr ImgDelayDescr, *PImgDelayDescr;
+
+struct ImgDelayDescr {
+    dword grAttrs;
+    ImageBaseOffset32 szName;
+    ImageBaseOffset32 phmod;
+    ImageBaseOffset32 pIAT;
+    ImageBaseOffset32 pINT;
+    ImageBaseOffset32 pBoundIAT;
+    ImageBaseOffset32 pUnloadIAT;
+    dword dwTimeStamp;
+};
+
+typedef struct VS_VERSION_INFO VS_VERSION_INFO, *PVS_VERSION_INFO;
+
+struct VS_VERSION_INFO {
+    word StructLength;
+    word ValueLength;
+    word StructType;
+    wchar16 Info[16];
+    byte Padding[2];
+    dword Signature;
+    word StructVersion[2];
+    word FileVersion[4];
+    word ProductVersion[4];
+    dword FileFlagsMask[2];
+    dword FileFlags;
+    dword FileOS;
+    dword FileType;
+    dword FileSubtype;
+    dword FileTimestamp;
+};
+
+typedef struct VarFileInfo VarFileInfo, *PVarFileInfo;
+
+struct VarFileInfo {
+    word wLength;
+    word wValueLength;
+    word wType;
+};
+
+typedef struct StringInfo StringInfo, *PStringInfo;
+
+struct StringInfo {
+    word wLength;
+    word wValueLength;
+    word wType;
+};
+
+typedef struct IMAGE_RESOURCE_DIR_STRING_U_22 IMAGE_RESOURCE_DIR_STRING_U_22, *PIMAGE_RESOURCE_DIR_STRING_U_22;
+
+struct IMAGE_RESOURCE_DIR_STRING_U_22 {
+    word Length;
+    wchar16 NameString[11];
+};
+
+typedef struct GuardCfgTableEntry GuardCfgTableEntry, *PGuardCfgTableEntry;
+
+struct GuardCfgTableEntry {
+    ImageBaseOffset32 Offset;
+};
+
+typedef struct IMAGE_DOS_HEADER IMAGE_DOS_HEADER, *PIMAGE_DOS_HEADER;
+
+struct IMAGE_DOS_HEADER {
+    char e_magic[2]; // Magic number
+    word e_cblp; // Bytes of last page
+    word e_cp; // Pages in file
+    word e_crlc; // Relocations
+    word e_cparhdr; // Size of header in paragraphs
+    word e_minalloc; // Minimum extra paragraphs needed
+    word e_maxalloc; // Maximum extra paragraphs needed
+    word e_ss; // Initial (relative) SS value
+    word e_sp; // Initial SP value
+    word e_csum; // Checksum
+    word e_ip; // Initial IP value
+    word e_cs; // Initial (relative) CS value
+    word e_lfarlc; // File address of relocation table
+    word e_ovno; // Overlay number
+    word e_res[4][4]; // Reserved words
+    word e_oemid; // OEM identifier (for e_oeminfo)
+    word e_oeminfo; // OEM information; e_oemid specific
+    word e_res2[10][10]; // Reserved words
+    dword e_lfanew; // File address of new exe header
+    byte e_program[64]; // Actual DOS program
+};
+
+typedef struct DotNetPdbInfo DotNetPdbInfo, *PDotNetPdbInfo;
+
+struct DotNetPdbInfo {
+    char signature[4];
+    GUID guid;
+    dword age;
+    char pdbpath[7];
+};
+
+
+
